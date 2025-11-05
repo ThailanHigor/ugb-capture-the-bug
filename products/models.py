33342ba7d4f16ef1afed3810dcad6d7a25e8186b
thailanhigor <thailan-higor@hotmail.com>
2025-11-05 19:07:28 -0300

    Desafio 1

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField("Nome", max_length=100)
+    description = models.TextField("Descrição", blank=True, default="")
+    price = models.DecimalField("Preço", max_digits=8, decimal_places=2)
+    active = models.BooleanField("Ativo", default=True)
+    created_at = models.DateTimeField("Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+        return f"{self.name} — {self.price}"
