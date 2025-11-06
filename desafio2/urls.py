9ef55b76920ba3d75cf67740bb6a559c2de6d6e7
Pedro Henrique Dornellas <pedrodornellas@id.uff.br>
2025-11-06 09:46:13 -0300

    Import do model no arquivo "views.py"

--- desafio2/urls.py
+++ desafio2/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('home/', views.index, name='product_list'),
 ]
